8d5340285579c232eefd70453ab90b6136a618bb
Vishwkant <62985421+vishwkant0693@users.noreply.github.com>
2023-10-05 17:18:19 +0530

    Update Header.js
    moving navigation links from page to header to make it more user friendly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,58 @@
 import React from 'react'
+import ScrollToSection from './scroll'
 
 const Header = (props) => {
+  const style = {
+    fontFamily: "'Poppins', sans-serif",
+    textAlign: "center"
+  }
   return (
     <>
-      <nav className={`navbar sticky-top p-3 mb-2 bg-${props.mode} text-${props.color} `} style={{fontFamily: "'Poppins', sans-serif"}}>
-        <div className="container d-flex justify-content-between " id='navbar'>
-            <div className='one'>
-              <img src="img/Portfolio.png" alt="Bootstrap" width="200" height="28" />
-            </div>
-            <div className="two">
-              <div className="form-check form-switch r-0 two">
-                <input className='form-check-input' type="checkbox" role="switch" onClick={props.toggleMode} id="flexSwitchCheckDefault"/>
-                  <label className="form-check-label" for="flexSwitchCheckDefault">Enable {props.color} Mode</label>
-              </div>
-            </div>
+      <nav className={`navbar navbar-expand-lg fixed-top p-1 mb-1 text-${props.color} `} style={{ fontFamily: "'Poppins', sans-serif", width: "100%", backgroundColor: props.mode === "light" ? "white" : "#0B0B45"  }} id='navbar'>
+        <div className="container-fluid d-flex justify-content-between " >
+
+          <div className='one'>
+            <img src="img/Portfolio.png" alt="Bootstrap" width="200" height="28" />
+          </div>
+          <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
+          <div style={style} >
+            <ul className="navbar nav nav-underline collapse navbar-collapse" id="navbarNavAltMarkup">
+              <ScrollToSection />
+              <li className="nav-item">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#qualification">Qualification</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">Skills</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#experience">Experience</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#get-in-touch">Get in touch</a>
+              </li>
+              <li>
+                <div className="two">
+                  <div className="form-check form-switch r-0 two">
+                    <input className='form-check-input' type="checkbox" role="switch" onClick={props.toggleMode} id="flexSwitchCheckDefault" />
+                    <label className="form-check-label" for="flexSwitchCheckDefault">Enable {props.color} Mode</label>
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+          </div>
         </div>
       </nav>
     </>
